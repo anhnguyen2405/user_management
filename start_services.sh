327d596928fe8e853e0ceb0ad75e8267af398dd2
anhnguyen2405 <ntnanh@tma.com.vn>
2023-08-07 10:10:14 +0700

    Scale microservices to 2 pods

--- start_services.sh
+++ start_services.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-# Build Docker images
-docker build -t auth_service ./auth_service
-docker build -t user_service ./user_service
-docker build -t ui_service ./ui_service
+# Set Kubernetes context to your EKS cluster
+# kubectl config use-context Cluster3
 
-# Start microservices using Docker Compose
-docker-compose up
+# Build Docker images
+docker build -t ntnanh/auth_service ./auth_service
+docker build -t ntnanh/user_service ./user_service
+docker build -t ntnanh/ui_service ./ui_service
+
+# # Push Docker images to a container registry (if needed)
+# docker push ntnanhw1/auth_service
+# docker push ntnanhw1/user_service
+# docker push ntnanhw1/ui_service
+
+# Apply Kubernetes deployments and services
+kubectl apply -f kubernetes/auth_service_deploy.yaml
+kubectl apply -f kubernetes/user_service_deploy.yaml
+kubectl apply -f kubernetes/ui_service_deploy.yaml
+
+kubectl apply -f kubernetes/auth_service_service.yaml
+kubectl apply -f kubernetes/user_service_service.yaml
+kubectl apply -f kubernetes/ui_service_service.yaml
